e94bebc20266c4fec081284fc660204bbaf2e0e8
Andre Almeida <a.almeida@unsw.edu.au>
2016-02-22 15:54:32 +1100

    Manage multiple data files per subject

--- SoundRefAB/data_file_io.py
+++ SoundRefAB/data_file_io.py
@@ -2,17 +2,17 @@ import tempfile
 import numpy as np
 import os 
 
-def store_temp_data_file(data,  subject_id=0):
-    filename=tempfile.gettempdir()+'subj%d_temp.npy'%(subject_id)
+def store_temp_data_file(data,  subject_id=0, suffix='temp'):
+    filename=tempfile.gettempdir()+'subj%d_%s.npy'%(subject_id, suffix)
     np.save(filename,data)
 
-def retrieve_temp_data_file( subject_id=0):
-    filename=tempfile.gettempdir()+'subj%d_temp.npy'%(subject_id)
+def retrieve_temp_data_file( subject_id=0, suffix='temp'):
+    filename=tempfile.gettempdir()+'subj%d_%s.npy'%(subject_id, suffix)
     data = np.load(filename)
     return data.tolist()
     
-def erase_temp_data_file(subject_id=0):
-    filename=tempfile.gettempdir()+'subj%d_temp.npy'%(subject_id)
+def erase_temp_data_file(subject_id=0, suffix='temp'):
+    filename=tempfile.gettempdir()+'subj%d_%s.npy'%(subject_id, suffix)
     try:
         os.remove(filename)
     except OSError:
